f68e220ed6a4c234438f8141e4472032d1d7d903
jdroland31 <jdroland31@yahoo.com>
2020-05-28 20:49:25 -0400

    Add files via upload

---
+++ assignment_1.1.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Jonathan Roland
+; Date: 28 May 2020
+; Modified By: Problem solver's name here
+; Description: A javascript program with at least 2 errors
+;===========================================
+*/
+
+Var one = 1;
+var two = 2;
+var three == 3;
+var four = 4
+var five! = 5;
+
+if(five - two = three){
+    console.log('variable values are correct.');
+}
+
+if(one + four === five){
+    console.log(variable values are correct.);
+}
