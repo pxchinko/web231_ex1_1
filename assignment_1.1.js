1fb65994e73140e42031339b7c99fe866a2301e0
Sarah Massie <smassie@my365.bellevue.edu>
2020-05-31 00:45:41 -0700

    Update assignment_1.1.js

--- assignment_1.1.js
+++ assignment_1.1.js
@@ -3,21 +3,21 @@
 ; Title: Assignment 1.1
 ; Author: Jonathan Roland
 ; Date: 28 May 2020
-; Modified By: Problem solver's name here
+; Modified By: Sarah Massie
 ; Description: A javascript program with at least 2 errors
 ;===========================================
 */
 
-Var one = 1;
+var one = 1; // lowercase v for the variable
 var two = 2;
-var three == 3;
+var three == 3; // removed double equal sign
 var four = 4
-var five! = 5;
+var five = 5; // removed the exclamation point
 
 if(five - two = three){
     console.log('variable values are correct.');
 }
 
-if(one + four === five){
+if(one + four = five){ // removed the double equal sign
     console.log(variable values are correct.);
 }
